6fa482dc5c926e4e109df2bc09fea39a1c03171b
Collin MacDonald <macd.collin@gmail.com>
2024-06-16 17:28:38 -0500

    Do some formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,39 +3,33 @@
 </script>
 
 <body class="flex flex-col justify-center items-center text-center pt-5 bg-slate-900 font-mono text-slate-200">
-
-  <Menubar.Root class="bg-slate-700">
-    <Menubar.Menu>
-      <Menubar.Trigger>Listen Now</Menubar.Trigger>
-      <Menubar.Content class="bg-slate-700 text-slate-200">
-      <Menubar.Item>
-        link to spotify
-      </Menubar.Item>
-      <Menubar.Item>
-        link to youtube
-      </Menubar.Item>
-      </Menubar.Content>
-    </Menubar.Menu>
-    <Menubar.Menu>
-      <Menubar.Trigger>Socials</Menubar.Trigger>
-      <Menubar.Content class="bg-slate-700 text-slate-200">
-      <Menubar.Item>
-        link to twitter?
-      </Menubar.Item>
-      <Menubar.Item>
-        email?
-      </Menubar.Item>
-      </Menubar.Content>
-    </Menubar.Menu>
-    <Menubar.Menu>
-      <Menubar.Trigger>About</Menubar.Trigger>
-      <Menubar.Content class="bg-slate-700 text-slate-200">
-      <Menubar.Item>
-        About
-      </Menubar.Item>
-      </Menubar.Content>
-    </Menubar.Menu>
-  </Menubar.Root>
+  <div class="flex items-center justify-center bg-slate-700 w-full">
+    <img src="/favicon.png" alt="1x Developer Podcast Logo" width="100px" class="p-4 absolute left-0"/>
+    <Menubar.Root class="flex-1 bg-slate-700">
+      <Menubar.Menu>
+        <Menubar.Trigger>Listen Now</Menubar.Trigger>
+        <Menubar.Content class="bg-slate-700 text-slate-200">
+          <Menubar.Item>
+            Spotify
+          </Menubar.Item>
+          <Menubar.Item>
+            YouTube
+          </Menubar.Item>
+        </Menubar.Content>
+      </Menubar.Menu>
+      <Menubar.Menu>
+        <Menubar.Trigger>Socials</Menubar.Trigger>
+        <Menubar.Content class="bg-slate-700 text-slate-200">
+          <Menubar.Item>
+            Twitter
+          </Menubar.Item>
+          <Menubar.Item>
+            Email
+          </Menubar.Item>
+        </Menubar.Content>
+      </Menubar.Menu>
+    </Menubar.Root>
+  </div>
   <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
     <h1 class="text-6xl">
         1x Developer Podcast
